bcc51ad36d583ef17bc180391cf1cf1ec0d4b16f
miladAhmadi3373 <miladahmadi1387pc@gmail.com>
2026-01-08 14:31:33 +0330

    Create Header

---
+++ components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+import styles from "@/layout/Header.module.css";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className="">
+        <ul>
+          <li>
+            <Link href={"/"}>صفحه اصلی</Link>
+          </li>
+          <li>
+            <Link href={"/buy-residentials"}>اگهی ها</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.login}>
+        <Link href={"/signin"}>
+          <FiLogIn />
+          <span>ورود</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
